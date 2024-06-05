4b61953f3a08d94a38db0273d02654e22279fa47
Jonathan <coding.with.johnny.dev@gmail.com>
2024-06-04 21:49:15 -0600

    feat: creating api and returning resources

---
+++ API/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+// [Route("api/ciudades")]
+public class CitiesController : BaseApiController
+{
+  [HttpGet]
+  public ActionResult<IEnumerable<CityDto>> GetCities()
+  {
+    return Ok(CitiesDataStore.Current.Cities);
+  }
+
+  [HttpGet("{id}")]
+  public ActionResult<CityDto> GetCityById(int id)
+  {
+    // find city
+    var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+    if (cityToReturn is null)
+    {
+      return NotFound($"City with id: {id} was not found");
+    }
+
+    return Ok(cityToReturn);
+  }
+
+  [HttpGet("Version")]
+  public ContentResult GetVersion()
+    => Content("v1.0.0");
+}
---
+++ API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using API.Controllers;
+using API.Models;
+
+namespace API;
+
+[Route("api/cities/{cityId}/[controller]")]
+public class PointsOfInterestController : BaseApiController
+{
+  [HttpGet]
+  public ActionResult<IEnumerable<PointOfInterestDto>> GetPointOfInterest(int cityId)
+  {
+    var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+    if (city is null)
+    {
+      return NotFound($"City with id: {cityId} was not found.");
+    }
+
+    return Ok(city.PointsOfInterest);
+  }
+
+  [HttpGet("{pointOfInterestId}")]
+  public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+  {
+    var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+    if (city is null)
+    {
+      return NotFound($"City with id: {cityId} was not found.");
+    }
+
+    var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+
+    if (pointOfInterest is null)
+    {
+      return NotFound($"Point of interest with id: {pointOfInterestId} was not found.");
+    }
+
+    return Ok(pointOfInterest);
+  }
+}
