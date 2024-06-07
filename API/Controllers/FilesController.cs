65c793f17126162a5ca0ee85fe1f655f3027472c
Jonathan <coding.with.johnny.dev@gmail.com>
2024-06-06 21:11:07 -0600

    feat: manipulating resources and validating input

--- API/Controllers/FilesController.cs
+++ API/Controllers/FilesController.cs
@@ -36,4 +36,28 @@ public class FilesController : BaseApiController
     return File(bytes, contentType, Path.GetFileName(pathToFile));
 
   }
+
+  [HttpPost]
+  public async Task<ActionResult> UploadFile(IFormFile file)
+  {
+    // Validate the input. Put a limit on file size to avoid large uploads attacks.
+    // Only accept .pdf files (check content type)
+    System.Console.WriteLine(file.Length);
+    System.Console.WriteLine(10 * 1024 * 1024);
+    if (file.Length == 0 || file.Length > 10 * 1024 * 1048 || file.ContentType != "application/pdf")
+    {
+      return BadRequest("No file or an invalid one has been inputted");
+    }
+
+    // Create the file path. Avoid using file. Filename , as an attacker can provide a
+    // malicious one, including full paths or relative paths.
+    var filePath = Path.Combine(Directory.GetCurrentDirectory(), $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+    using (var stream = new FileStream(filePath, FileMode.Create))
+    {
+      await file.CopyToAsync(stream);
+    }
+
+    return Ok("Your file has been uploaded successfully");
+  }
 }
--- API/Controllers/PointsOfInterestController.cs
+++ API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.JsonPatch;
 
 using API.Controllers;
 using API.Models;
@@ -39,5 +40,125 @@ public class PointsOfInterestController : BaseApiController
     }
 
     return Ok(pointOfInterest);
+  }
+
+  [HttpPost]
+  public ActionResult<PointOfInterestDto> CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+  {
+    var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+    if (city is null)
+    {
+      return NotFound($"City with id: {cityId} was not found.");
+    }
+
+    /* demo purposes - to be improved */
+    var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+    var finalPointOfInterest = new PointOfInterestDto
+    {
+      Id = maxPointOfInterestId + 1,
+      Name = pointOfInterest.Name,
+      Description = pointOfInterest.Description
+    };
+
+    city.PointsOfInterest.Add(finalPointOfInterest);
+
+    // return CreatedAtRoute
+    return CreatedAtAction(
+      nameof(GetPointOfInterest),
+      new
+      {
+        cityId,
+        pointOfInterestId = finalPointOfInterest.Id
+      },
+      finalPointOfInterest
+    );
+  }
+
+  [HttpPut("{pointOfInterestId}")]
+  public ActionResult UpdatePointOfInterest(int cityId, int pointOfInterestId, [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+  {
+    var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+    if (city is null)
+    {
+      return NotFound($"City with id: {cityId} was not found.");
+    }
+
+    var pointOfInterestToUpdate = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+
+    if (pointOfInterestToUpdate is null)
+    {
+      return NotFound($"Point of interest with id: {pointOfInterestId} was not found.");
+    }
+
+    pointOfInterestToUpdate.Name = pointOfInterest.Name;
+    pointOfInterestToUpdate.Description = pointOfInterest.Description;
+
+    return NoContent();
+  }
+
+  [HttpPatch("{pointOfInterestId}")]
+  public ActionResult PartiallyUpdatePointOfInterest(int cityId, int pointOfInterestId, JsonPatchDocument<PointOfInterestForUpdateDto> patchDocument)
+  {
+    var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+    if (city is null)
+    {
+      return NotFound();
+    }
+
+    var pointOfInterestToUpdate = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+
+    if (pointOfInterestToUpdate is null)
+    {
+      return NotFound($"Point of interest with id: {pointOfInterestId} was not found.");
+    }
+
+    var pointOfInterestToPatch = new PointOfInterestForUpdateDto
+    {
+      Name = pointOfInterestToUpdate.Name,
+      Description = pointOfInterestToUpdate.Description
+    };
+
+    patchDocument.ApplyTo(pointOfInterestToPatch, ModelState);
+
+    if (!ModelState.IsValid)
+    {
+      return ValidationProblem(ModelState);
+    }
+
+    if (!TryValidateModel(pointOfInterestToPatch))
+    {
+      return ValidationProblem(ModelState);
+    }
+
+    pointOfInterestToUpdate.Name = pointOfInterestToPatch.Name;
+    pointOfInterestToUpdate.Description = pointOfInterestToPatch.Description;
+
+    return NoContent();
+  }
+
+  [HttpDelete("{pointOfInterestId}")]
+  public ActionResult DeletePointOfInterest(int cityId, int pointOfInterestId)
+  {
+    var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+    if (city is null)
+    {
+      return NotFound($"City with id: {cityId} was not found.");
+    }
+
+    var pointOfInterestToDelete = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+
+    if (pointOfInterestToDelete is null)
+    {
+      return NotFound($"Point of interest with id: {pointOfInterestId} was not found.");
+    }
+
+    city.PointsOfInterest.Remove(pointOfInterestToDelete);
+
+    return NoContent();
   }
 }
